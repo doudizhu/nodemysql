c92fe0b6520e656725ef929b9bb1f4536e264c68
lipeng <lipeng@daokoudai.com>
2019-10-25 16:42:06 +0800

    v 0.0.1 创建表：增、查

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  // database : 'my_db'
+  database : 'nodemysql'
 });
 
 // Connect
@@ -24,6 +24,56 @@ app.get('/createdb', (req, res) => {
     if (err) throw err;
     console.log(result);
     res.send('Database created...');
+  });
+});
+
+// 创建表
+app.get('/createpoststable',(req,res)=>{
+  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255),PRIMARY KEY(id))'
+  db.query(sql,(err,result)=>{
+    if(err)throw err;
+    console.log(result)
+    res.send('posts表已经建立...')
+  })
+})
+
+// 插入内容
+app.get('/addpost1', (req, res) => {
+  let post = { title: 'post one', body: 'this is post one' };
+  let sql = 'INSERT INTO posts SET ?';
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('post 1 added...');
+  });
+});
+app.get('/addpost2', (req, res) => {
+  let post = { title: 'post two', body: 'this is post two' };
+  let sql = 'INSERT INTO posts SET ?';
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('post 1 added...');
+  });
+});
+
+// 查询内容
+app.get('/getposts', (req, res) => {
+  let sql = 'SELECT * FROM posts';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    // res.send('查询成功...');
+    res.json(result)
+  });
+});
+// 查询单条内容
+app.get('/getposts/:id', (req, res) => {
+  let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result)
   });
 });
 
